a831a7593930eebd00abcd5b01e690ce11ef2965
edgwatson1 <e@edwardwatson.com>
2020-01-07 18:15:14 +0000

    css main style

--- src/Learners/QuizList/QuizList.js
+++ src/Learners/QuizList/QuizList.js
@@ -1,11 +1,21 @@
-import React from "react"
+import React from "react";
+import { Link } from "react-router-dom";
 
 const QuizList = () => {
+  return (
+    <>
+      <div>
+        <h1>Available Quizzes</h1>
+        <hr />
+        <h2>Zomato Gold</h2>
+        <p>Short description about the quiz and why you should do it.</p>
+        <Link to="/Learners/Quiz/Quiz">
+          <a href="/Learners/Quiz/Quiz">Take Quiz ►</a>
+        </Link>
 
-    return(
-        <div>
-            <p> You are on QuizList</p>
-        </div>
-    )
-}
-export default QuizList; 
+        <a href="/Learners/Quiz/Quiz">Documentation ►</a>
+      </div>
+    </>
+  );
+};
+export default QuizList;
