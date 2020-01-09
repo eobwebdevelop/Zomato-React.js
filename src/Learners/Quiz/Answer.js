34c4d7ad8160affc0a2de999115de69b7632fea7
elenaOrtega <elenabaura56@gmail.com>
2020-01-09 13:02:52 +0000

    littleChange

--- src/Learners/Quiz/Answer.js
+++ src/Learners/Quiz/Answer.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
-import { Container, Row}  from 'react-bootstrap';
-import { MDBContainer, MDBInput } from "mdbreact";
+import { Container, Row, Col}  from 'react-bootstrap';
+import { MDBInput } from "mdbreact";
 import './Quiz.css';
 
 
 
   class Answer extends Component {
-    
-    state = {
-      radio: 2,
-      correctAns: '',
-      an1: 'Answer by default1',
-      an2: 'Answer by default2',
-      an3: 'Answer by default3',
-      an4: 'Answer by default4'
+    constructor(props) {
+      super(props);
+      this.state = {
+        radio: null,
+        rightAns: '',
+        an1: 'Answer by default1',
+        an2: 'Answer by default2',
+        an3: 'Answer by default3',
+        an4: 'Answer by default4',
+        currentQuestion: 0,
+        totalQuestions: 10,
+        question: 'by default: How many complimentary dishes do Zomato Gold customers receive?',
+        timer: 'this : timer'
+      
+      }
+
     }
     
     onClick = (nr) => () => {
@@ -24,16 +32,43 @@ import './Quiz.css';
 
     render() {
       return (
-        <MDBContainer className="mt-5">
-          <MDBInput gap onClick={this.onClick(1)} checked={this.state.radio===1 ? true : false} label={this.state.an1} type="radio"
-            id="radio1" />
-          <MDBInput gap onClick={this.onClick(2)} checked={this.state.radio===2 ? true : false} label={this.state.an2} type="radio"
-            id="radio2" />
-          <MDBInput gap onClick={this.onClick(3)} checked={this.state.radio===3 ? true : false} label={this.state.an3} type="radio"
-            id="radio3" />
-          <MDBInput gap onClick={this.onClick(4)} checked={this.state.radio===4 ? true : false} label={this.state.an4} type="radio"
-            id="radio4" />
-        </MDBContainer>
+       <>
+        <Container>
+           <h1>Zomato Gold Quiz</h1>
+                <hr />
+           {/* Counter */}
+          
+            <span className='counter'> Question {this.state.currentQuestion} from  {this.state.totalQuestions}</span>
+           
+           {/* Question  */}
+            <p className='questionStyle'>{this.state.question}</p>
+
+            <MDBInput gap onClick={this.onClick(1)} checked={this.state.radio===1 ? true : false} label={this.state.an1} type="radio"
+              id="radio1" />
+            <MDBInput gap onClick={this.onClick(2)} checked={this.state.radio===2 ? true : false} label={this.state.an2} type="radio"
+              id="radio2" />
+            <MDBInput gap onClick={this.onClick(3)} checked={this.state.radio===3 ? true : false} label={this.state.an3} type="radio"
+              id="radio3" />
+            <MDBInput gap onClick={this.onClick(4)} checked={this.state.radio===4 ? true : false} label={this.state.an4} type="radio"
+              id="radio4" />
+
+            {/* Timer */}
+            <p className='timer'> {this.state.timer}</p>
+
+            {/* Bottons */}
+            <Row>
+              <Col md='12'>
+                <div className="d-flex justify-content-end">
+                  <button type="submit" class="btn disabled">
+                    Next
+                  </button>
+                </div>
+              </Col>
+            </Row>
+
+
+          </Container>
+        </>
         );
       }
     }
--- src/Learners/Quiz/Question.js
+++ src/Learners/Quiz/Question.js
@@ -4,12 +4,19 @@ import { Container, Row}  from 'react-bootstrap';
 
 
 class Question extends Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    question: 'by default: How many complimentary dishes do Zomato Gold customers receive?'
+  }
+}  
 
 render() {
   return (
-        <Container>
-            <p>You are in Question</p>
-        </Container>
+      <>
+      {/* Counter */}
+        <p className='questionStyle'>{this.state.question}</p>
+      </>
     );
   }
 }
