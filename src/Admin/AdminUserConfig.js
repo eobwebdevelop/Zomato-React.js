6a23fe3639b551eb12d757f962d1f5cd08e00baa
edgwatson1 <e@edwardwatson.com>
2020-01-09 12:59:39 +0000

    update

---
+++ src/Admin/AdminUserConfig.js
@@ -0,0 +1,244 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const AdminUserConfig = () => {
+  return (
+    <div>
+      <Container>
+        <h1>Manage Users</h1>
+        <p>You are viewing a list of users.</p>
+        <Link to="/Admin/AdminQuizMaker">
+          <button type="submit" class="btn">
+            Add User
+          </button>
+        </Link>
+        <br />
+        <Link to="/">
+          <button type="submit" class="btn">
+            Export Data
+          </button>
+        </Link>
+
+        <table class="tftable" border="1">
+          <tr>
+            <th>User Type</th>
+            <th>User ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Region</th>
+            <th>Phone Number</th>
+            <th>Delete User?</th>
+          </tr>
+          <tr>
+            <td>Restauranter</td>
+            <td>1</td>
+            <td>Britney</td>
+            <td>Spears</td>
+            <td>britney@hotmail.com</td>
+            <td>Lisbon</td>
+            <td>919192828</td>
+            <td>
+              <a
+                class="view-quizzes-page-links-side-by-side"
+                href="/Learners/Quiz/Quiz"
+              >
+                Delete User ►
+              </a>
+            </td>
+          </tr>
+
+          <tr>
+            <td>Restauranter</td>
+            <td>1</td>
+            <td>Britney</td>
+            <td>Spears</td>
+            <td>britney@hotmail.com</td>
+            <td>Lisbon</td>
+            <td>919192828</td>
+            <td>
+              <a
+                class="view-quizzes-page-links-side-by-side"
+                href="/Learners/Quiz/Quiz"
+              >
+                Delete User ►
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>Restauranter</td>
+            <td>1</td>
+            <td>Britney</td>
+            <td>Spears</td>
+            <td>britney@hotmail.com</td>
+            <td>Lisbon</td>
+            <td>919192828</td>
+            <td>
+              <a
+                class="view-quizzes-page-links-side-by-side"
+                href="/Learners/Quiz/Quiz"
+              >
+                Delete User ►
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>Restauranter</td>
+            <td>1</td>
+            <td>Britney</td>
+            <td>Spears</td>
+            <td>britney@hotmail.com</td>
+            <td>Lisbon</td>
+            <td>919192828</td>
+            <td>
+              <a
+                class="view-quizzes-page-links-side-by-side"
+                href="/Learners/Quiz/Quiz"
+              >
+                Delete User ►
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>Restauranter</td>
+            <td>1</td>
+            <td>Britney</td>
+            <td>Spears</td>
+            <td>britney@hotmail.com</td>
+            <td>Lisbon</td>
+            <td>919192828</td>
+            <td>
+              <a
+                class="view-quizzes-page-links-side-by-side"
+                href="/Learners/Quiz/Quiz"
+              >
+                Delete User ►
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>Restauranter</td>
+            <td>1</td>
+            <td>Britney</td>
+            <td>Spears</td>
+            <td>britney@hotmail.com</td>
+            <td>Lisbon</td>
+            <td>919192828</td>
+            <td>
+              <a
+                class="view-quizzes-page-links-side-by-side"
+                href="/Learners/Quiz/Quiz"
+              >
+                Delete User ►
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>Restauranter</td>
+            <td>1</td>
+            <td>Britney</td>
+            <td>Spears</td>
+            <td>britney@hotmail.com</td>
+            <td>Lisbon</td>
+            <td>919192828</td>
+            <td>
+              <a
+                class="view-quizzes-page-links-side-by-side"
+                href="/Learners/Quiz/Quiz"
+              >
+                Delete User ►
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>Restauranter</td>
+            <td>1</td>
+            <td>Britney</td>
+            <td>Spears</td>
+            <td>britney@hotmail.com</td>
+            <td>Lisbon</td>
+            <td>919192828</td>
+            <td>
+              <a
+                class="view-quizzes-page-links-side-by-side"
+                href="/Learners/Quiz/Quiz"
+              >
+                Delete User ►
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>Restauranter</td>
+            <td>1</td>
+            <td>Britney</td>
+            <td>Spears</td>
+            <td>britney@hotmail.com</td>
+            <td>Lisbon</td>
+            <td>919192828</td>
+            <td>
+              <a
+                class="view-quizzes-page-links-side-by-side"
+                href="/Learners/Quiz/Quiz"
+              >
+                Delete User ►
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>Restauranter</td>
+            <td>1</td>
+            <td>Britney</td>
+            <td>Spears</td>
+            <td>britney@hotmail.com</td>
+            <td>Lisbon</td>
+            <td>919192828</td>
+            <td>
+              <a
+                class="view-quizzes-page-links-side-by-side"
+                href="/Learners/Quiz/Quiz"
+              >
+                Delete User ►
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>Restauranter</td>
+            <td>1</td>
+            <td>Britney</td>
+            <td>Spears</td>
+            <td>britney@hotmail.com</td>
+            <td>Lisbon</td>
+            <td>919192828</td>
+            <td>
+              <a
+                class="view-quizzes-page-links-side-by-side"
+                href="/Learners/Quiz/Quiz"
+              >
+                Delete User ►
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>Restauranter</td>
+            <td>1</td>
+            <td>Britney</td>
+            <td>Spears</td>
+            <td>britney@hotmail.com</td>
+            <td>Lisbon</td>
+            <td>919192828</td>
+            <td>
+              <a
+                class="view-quizzes-page-links-side-by-side"
+                href="/Learners/Quiz/Quiz"
+              >
+                Delete User ►
+              </a>
+            </td>
+          </tr>
+        </table>
+      </Container>
+    </div>
+  );
+};
+export default AdminUserConfig;
--- src/AdminNav.js
+++ src/AdminNav.js
@@ -1,25 +1,30 @@
 import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import DropdownSelector from "./DropdownSelector";
+
+import "./AdminNav.css";
 
 const AdminNav = () => {
   return (
     <div>
-      <ul>
-        <p>
-          <a href="/Admin/AdminApp">Admin Navigator</a>
-        </p>
-        <li>
-          <a href="/Admin/DocHost">DocHost</a>
-        </li>
-        <li>
-          <a href="/Admin/QA">QA</a>
-        </li>
-        <li>
-          <a href="/Admin/QuizMaker">Quiz Maker</a>
-        </li>
-        <li>
-          <a href="/Admin/QuizUpdate">Quiz Update</a>
-        </li>
-      </ul>
+      {/* Example */}
+      <Navbar bg="tyrolean" expand="lg">
+        <Navbar.Brand href="/Learners/QuizList/QuizList">
+          <img src="https://res.cloudinary.com/dpjc4trmq/image/upload/c_scale,w_130/v1578418844/i1fqkqecbismsg8o7w7b.png" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto navlinks">
+            <Nav.Link href="/Admin/AdminAppLogIn">Login</Nav.Link>
+            <Nav.Link href="/Admin/AdminQuizList">Manage Quizzes</Nav.Link>
+            <Nav.Link href="/Admin/AdminUserConfig">Manage Users</Nav.Link>
+            <Nav.Link href="/Admin/AdminAppLogIn">Log Out</Nav.Link>
+            <Nav.Link href="/Learners/Login/Login">Learner Portal</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <DropdownSelector />
+      </Navbar>
     </div>
   );
 };
