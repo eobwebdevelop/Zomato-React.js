bd05f42d692d13a20c0d82b839c8cd3570f62341
edgwatson1 <e@edwardwatson.com>
2020-01-09 15:50:44 +0000

    new sign up form

--- src/Learners/SignUp/SignUp.js
+++ src/Learners/SignUp/SignUp.js
@@ -1,6 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import RegionDropDown from "./RegionDropDown.js";
+
+const Regions = [
+  {
+    key: "Greater Lisbon",
+    text: "Greater Lisbon",
+    value: "Greater Lisbon"
+  },
+  {
+    key: "Porto",
+    text: "Porto",
+    value: "Porto"
+  },
+  {
+    key: "Algarve",
+    text: "Algarve",
+    value: "Algarve"
+  }
+];
+
+const UserType = [
+  {
+    key: "Restauranter",
+    text: "Restauranter",
+    value: "Restauranter"
+  },
+  {
+    key: "Restauraunt Employee",
+    text: "Restauraunt Employee",
+    value: "Restauraunt Employee"
+  },
+  {
+    key: "Zomato HQ Employee",
+    text: "Zomato HQ Employee",
+    value: "Zomato HQ Employee"
+  }
+];
 
 const SignUp = () => {
   return (
@@ -9,19 +46,33 @@ const SignUp = () => {
         <div class="formparentcontainer">
           <h1 id="h1-login"> Sign-Up </h1>
           <hr />
-          <form>
-            <input type="text" title="name" placeholder=" Name*" />
-            <br />
+          <form class="signup-form">
+            <h5>Personal Details:</h5>
+            <input type="text" title="firstname" placeholder=" First Name*" />
+            <input type="text" title="lastname" placeholder=" Last Name*" />
             <input type="text" title="email" placeholder=" Email*" />
+            <br /> <br />
+            <h5>Location:</h5>
+            <RegionDropDown selectOptions={Regions} placeholder="Your region" />
             <br />
-            <input type="text" title="email" placeholder=" Restaurant" />
-            <br />
-            <input type="password" title="password" placeholder=" Password" />
+            <h5>Zomato Information:</h5>
+            <RegionDropDown
+              selectOptions={UserType}
+              placeholder="Are you a... *"
+            />
+            <input
+              type="text"
+              title="email"
+              placeholder=" Restaurant (If applicable)"
+            />
+            <br /> <br />
+            <h5>Password:</h5>
+            <input type="password" title="password" placeholder=" Password*" />
             <br />
             <input
               type="password"
               title="password"
-              placeholder=" Confirm Password"
+              placeholder=" Confirm Password*"
             />
             <br />
             <Link to="/Learners/QuizList/QuizList">
@@ -29,7 +80,6 @@ const SignUp = () => {
                 Sign-Up
               </button>
             </Link>
-            <br />
           </form>
         </div>
       </Container>
