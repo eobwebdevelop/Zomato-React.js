f7f2aeba5fc0241576a8a76eb79e6b06c802a92a
edgwatson1 <e@edwardwatson.com>
2020-01-09 13:13:35 +0000

    update

--- src/AdminNav.js
+++ src/AdminNav.js
@@ -8,15 +8,17 @@ const AdminNav = () => {
   return (
     <div>
       {/* Example */}
-      <Navbar bg="tyrolean" expand="lg">
+      <Navbar id="navbar-admin" bg="black" expand="lg">
         <Navbar.Brand href="/Learners/QuizList/QuizList">
-          <img src="https://res.cloudinary.com/dpjc4trmq/image/upload/c_scale,w_130/v1578418844/i1fqkqecbismsg8o7w7b.png" />
+          <img src="https://res.cloudinary.com/edwardwatson/image/upload/c_scale,w_130/v1578575042/zomatologo_jhiglb.png" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto navlinks">
-            <Nav.Link href="/Admin/AdminAppLogIn">Login</Nav.Link>
+          <Nav className="mr-auto navlinks navbar-link-admin">
+            <Nav.Link id="navbar-link-admin" href="/Admin/AdminAppLogIn">
+              Login
+            </Nav.Link>
             <Nav.Link href="/Admin/AdminQuizList">Manage Quizzes</Nav.Link>
             <Nav.Link href="/Admin/AdminUserConfig">Manage Users</Nav.Link>
             <Nav.Link href="/Admin/AdminAppLogIn">Log Out</Nav.Link>
