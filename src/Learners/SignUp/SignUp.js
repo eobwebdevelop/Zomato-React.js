acd869ad57cc2e8f5d42834adc7058eb25c62a36
elenaOrtega <elenabaura56@gmail.com>
2020-01-14 17:33:50 +0000

    connection

--- src/Learners/SignUp/SignUp.js
+++ src/Learners/SignUp/SignUp.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from 'react';
 import { Link } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import RegionDropDown from "./RegionDropDown.js";
@@ -39,51 +39,107 @@ const UserType = [
   }
 ];
 
-const SignUp = () => {
-  return (
-    <div>
-      <Container>
-        <div class="formparentcontainer">
-          <h1 id="h1-login"> Sign-Up </h1>
-          <hr />
-          <form class="signup-form">
-            <h5>Personal Details:</h5>
-            <input type="text" title="firstname" placeholder=" First Name*" />
-            <input type="text" title="lastname" placeholder=" Last Name*" />
-            <input type="text" title="email" placeholder=" Email*" />
-            <br /> <br />
-            <h5>Location:</h5>
-            <RegionDropDown selectOptions={Regions} placeholder="Your region" />
-            <br />
-            <h5>Zomato Information:</h5>
-            <RegionDropDown
-              selectOptions={UserType}
-              placeholder="Are you a... *"
-            />
-            <input
-              type="text"
-              title="email"
-              placeholder=" Restaurant (If applicable)"
-            />
-            <br /> <br />
-            <h5>Password:</h5>
-            <input type="password" title="password" placeholder=" Password*" />
-            <br />
-            <input
-              type="password"
-              title="password"
-              placeholder=" Confirm Password*"
-            />
-            <br />
-            <Link to="/Learners/QuizList/QuizList">
-              <button type="submit" class="btn-login">
-                Sign-Up
-              </button>
-            </Link>
-          </form>
-        </div>
-      </Container>
-    </div>
-  );
-};
+class SignUp extends Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      first_name: 'elena',
+      last_name: 'ortega',
+      email: 'elena@hola.com',
+      password: 'holi',
+      phone_number: '99333883',
+      user_type_id: '2',
+      region_id: '1'
+
+    }
+  }
+
+  updateFirstname = (event) => {
+    this.setState({first_name: event.target.value})
+  }
+  updateLastName = (event) => {
+    this.setState({last_name: event.target.value})
+  }
+  updateEmail = (event) => {
+    this.setState({email: event.target.value})
+  }
+  updateNumber = (event) => {
+    this.setState({phone_number: event.target.value})
+  }
+
+  //Fix the selectors
+  updateRegionId = (event) => {
+    this.setState({region_id: event.target.value})
+  }
+  updateUserType = (event) => {
+    this.setState({user_type_id: event.target.value})
+  }
+
+  handlerSubmit = (e) => {
+    e.preventDefault();
+    console.log("the form has been submited with these fields:", this.state );
+    fetch("http://localhost:3000/auth/signup",
+    {
+        method:  'POST',
+        headers:  new Headers({
+                'Content-Type':  'application/json'
+        }),
+        body:  JSON.stringify(this.state),
+    })
+    .then(res  =>  res.json())
+    .then(
+        res  =>  this.setState({"flash":  res.flash}),
+        err  =>  this.setState({"flash":  err.flash})
+    )
+  }
+
+
+  render() {
+    return (
+      <>
+        <Container>
+          <div class="formparentcontainer">
+            <h1 id="h1-login"> Sign-Up </h1>
+            <hr />
+            <form class="signup-form" onSubmit={this.handlerSubmit}>
+              <h5>Personal Details:</h5>
+              <input type="text" name="firstname" placeholder=" First Name*" required onChange={this.updateFirstname} /> 
+              <input type="text" name="lastname" placeholder=" Last Name*"  required onChange={this.updateLastName}/>
+              <input type="email" name="email" placeholder=" Email*" required onChange={this.updateEmail}/>
+              <input type="text" name="phone" placeholder=" Phone Number" onChange={this.updateNumber}/>
+              <br /> <br />
+              <h5>Location:</h5>
+              <RegionDropDown selectOptions={Regions} onChange={this.updateUserType} placeholder=" Your region" />
+              <br />
+              <h5>Zomato Information:</h5>
+              <RegionDropDown
+                selectOptions={UserType}
+                placeholder=" Are you a... *"
+                onChange={this.updateUserType}
+              />
+              <input
+                type="text"
+                name="restaurant"
+                placeholder=" Restaurant (If applicable)"
+              />
+              <br /> <br />
+              <h5>Password:</h5>
+              <input type="password" name="password" placeholder=" Password*" />
+              <br />
+              <input
+                type="password"
+                name="password"
+                placeholder="Confirm Password*"
+              />
+              <br />
+                <button type="submit" class="btn-login">
+                  Sign-Up
+                </button>
+            </form>
+          </div>
+        </Container>
+      </>
+    );
+  }
+}
 export default SignUp;
