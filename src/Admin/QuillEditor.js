b8a5ba64c39e1abdc58b165b56ceeac5ef5d2a2d
Alexandra Santos <alexandraps17@gmail.com>
2020-01-17 17:10:47 +0000

    upload to cloudinary

--- src/Admin/QuillEditor.js
+++ src/Admin/QuillEditor.js
@@ -1,103 +1,84 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import ReactQuill, { Quill } from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-// import { cloudinaryPreset, apiBaseUrl } from '../keys'
+import 'quill/dist/quill.snow.css';
+import imageUpload from 'quill-plugin-image-upload';
 
-class QuillEditor extends Component {
-  constructor(props) {
-    super(props);
+// Quill.register('modules/imageUpload', imageUpload);
 
-    this.state = {
-      text: '',
-      imageUrl: [],
-      imageCloudinary: '',
-      loading : false
-    }; // You can also pass a Quill Delta here
-    
-  }
+const QuillEditor = () => {
+  const [text, setText] = useState('');
+  const [image, setImage] = useState('');
+  const [image64, setImage64] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  modules = {
-    toolbar: [
-      [{ 'header': [1, 2, false] }],
-      ['bold', 'italic', 'underline','strike', 'blockquote'],
-      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-      ['link', 'image'],
-      ['clean']
-    ],
-  }
-
-  handleChange = (value, delta) => {
-    const getImageUrl = (dlt) =>
-      dlt.ops.filter(i => i.insert && i.insert.image).map(i => i.insert);
-
-    this.setState({
-      text: value,
-      imageUrl: getImageUrl(delta)
-    });
-  }
-
-  uploadImage = async e => {
-    const files = e.target.files
-    const data = new FormData()
-
-    data.append('file', files[0])
-    data.append('upload_preset', 'Zomato')
-
-    this.setState({
-      loading: true
-    }, () => {
-      fetch('https://api.cloudinary.com/v1_1/ddoc8nfxb/image/upload',
-        {
-          method: 'POST',
-          mode: 'no-cors',
-          body: data
-        }
-      )
-      .then(res => res.json())
-      .then(data => {
-        console.log('upload complete', data)
-  
-        this.setState({
-          image: data.secure_url,
-          loading: false
-        });
-      })
-    });
-
-
-  }
-
-  render() {
-    console.log(this.state)
-    return (
-        <div className="text-editor">
-          <ReactQuill 
-            theme="snow"
-            modules={this.modules}
-            formats={this.formats}
-            value={this.state.text}
-            onChange={this.handleChange}
-          />
-
-          <h1> test</h1>
-
-          <div>
-      <h1>Upload Image</h1>
-      <input
-        type="file"
-        name="file"
-        placeholder="Upload an image"
-        onChange={this.uploadImage}
-      />
-      {this.state.loading ? (
-        <h3>Loading...</h3>
-      ) : (
-        <img src={this.state.imageCloudinary} style={{ width: '300px' }} />
-      )}
-    </div>
-        </div>
+  const uploadImage = async (e) => {
+    const { files } = e.target;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'Zomato');
+    setLoading(true);
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/ddoc8nfxb/image/upload',
+      {
+        method: 'POST',
+        body: data,
+      },
     );
-  }
-}
+    const file = await res.json();
+
+    setImage(file.secure_url);
+    setLoading(false);
+  };
+
+  const handleChange = (value, delta) => {
+    console.log('delta', delta.ops[0].insert.image, 'value', value);
+    const getImage64 = (dlt) => {
+      dlt.ops.filter((i) => i.insert && i.insert.image).map((i) => console.log('i.insert', i.insert));
+    };
+    setText(value);
+    // setImage64(getImage64(delta));
+  };
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
+      ['link', 'image'],
+      ['clean'],
+    ],
+    // imageUpload: {
+    //   upload: uploadImage(),
+    // },
+  };
+
+// console.log('image64', image64);
+  return (
+    <div className="text-editor">
+      <ReactQuill
+        theme="snow"
+        modules={modules}
+        value={text}
+        onChange={handleChange}
+      />
+
+      <div className="App">
+        <h1>Upload Image</h1>
+        <input
+          type="file"
+          name="file"
+          placeholder="Upload an image"
+          onChange={uploadImage}
+        />
+        {loading ? (
+          <h3>Loading...</h3>
+        ) : (
+          <img src={image} style={{ width: '300px' }} alt="uploaded" />
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default QuillEditor;
--- src/keys.js
+++
@@ -1,6 +0,0 @@
-// Cloudinary keys
-
-const cloudinaryPreset = 'Zomato';
-const apiBaseUrl = 'https://api.cloudinary.com/v1_1/ddoc8nfxb';
-
-export default { cloudinaryPreset, apiBaseUrl };
