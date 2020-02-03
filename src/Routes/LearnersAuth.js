80418e1f1c9b6be681b4050e88fedd08877cfe93
elenaOrtega <elenabaura56@gmail.com>
2020-02-03 18:27:12 +0000

    routesAuthLearners

---
+++ src/Routes/LearnersAuth.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  Switch, Route, Redirect, Link,
+} from 'react-router-dom';
+// Learners SignUp
+import SignUp from '../Learners/SignUp/SignUp';
+import BasicNav from '../BasicNav.js';
+import LogIn from '../Learners/LogIn/LogIn';
+
+
+const LearnersAuth = (props) => (
+  <>
+    <Switch>
+      <Route
+        exact
+        path="/"
+        render={() => <Redirect to="/learners/login" />}
+      />
+      <Route
+        exact
+        path="/learners/signup"
+        render={() => (
+          <>
+            <BasicNav />
+            <SignUp restaurants={props.restaurants} />
+          </>
+        )}
+      />
+      <Route
+        exact
+        path="/learners/login"
+        render={() => (
+          <>
+            <BasicNav />
+            <LogIn />
+          </>
+        )}
+      />
+    </Switch>
+  </>
+);
+
+export default LearnersAuth;
