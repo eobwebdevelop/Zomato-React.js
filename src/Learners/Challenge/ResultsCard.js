211a98891ed26d93a3a7c393b6d779d91e3e7969
Edward Watson <e@edwardwatson.com|>
2020-02-03 14:50:32 +0000

    fix_quiz_scoring

--- src/Learners/Challenge/ResultsCard.js
+++ src/Learners/Challenge/ResultsCard.js
@@ -2,7 +2,6 @@ import React from "react";
 // import LanguagesContext, { availableLanguages } from '../../contexts/languages-context';
 // import translations from '../../i18n/translations';
 
-
 class ResultsCard extends React.Component {
   render() {
     const {
@@ -11,22 +10,22 @@ class ResultsCard extends React.Component {
       questionNumber,
       questionText,
       userAnswerText,
-      userAnswerID,
+      userAnswerID
     } = this.props;
 
-    if (userAnswerID === correctAnswerID) {
+    if (userAnswerText === correctAnswerText) {
       return (
         <>
           <h1>
-            Question
-            {' '}
-            {questionNumber}
-:
-            {' '}
-            {questionText}
+            Question {questionNumber}: {questionText}
           </h1>
 
-          <h2>Correct<span role="img" aria-label="jsx-a11y/accessible-emoji">✅</span> </h2>
+          <h2>
+            Correct
+            <span role="img" aria-label="jsx-a11y/accessible-emoji">
+              ✅
+            </span>{" "}
+          </h2>
           <p>
             Your answer:
             {userAnswerText}
@@ -37,20 +36,20 @@ class ResultsCard extends React.Component {
     return (
       <>
         <h1>
-            Question
-          {' '}
-          {questionNumber}
-:
-          {' '}
-          {questionText}
+          Question {questionNumber}: {questionText}
         </h1>
-        <h2>Incorrect <span role="img" aria-label="jsx-a11y/accessible-emoji">❌</span></h2> 
+        <h2>
+          Incorrect{" "}
+          <span role="img" aria-label="jsx-a11y/accessible-emoji">
+            ❌
+          </span>
+        </h2>
         <p>
-Your answer:
+          Your answer:
           {userAnswerText}
         </p>
         <p>
-The correct answer:
+          The correct answer:
           {correctAnswerText}
         </p>
       </>
