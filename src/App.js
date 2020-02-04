7fce12a09b3268376647157ff5af6b46d5956a6d
Edward Watson <e@edwardwatson.com|>
2020-02-04 11:40:09 +0000

    fix scoring refresh issue

--- src/App.js
+++ src/App.js
@@ -268,7 +268,7 @@ class App extends Component {
       ) {
         totalScore = totalScore + 1;
       }
-      console.log("score at present", this.state.score)
+    
     }
 
     this.setState({ score: totalScore });
@@ -284,7 +284,7 @@ class App extends Component {
     // This is called on results page, and also required to be in ComponerntDidMount on the homepage to refresh quiz-related state variables should the user click out / navigate from a quiz in play.
     // console.log("refresh");
     this.stopTimer();
-    this.setState({ overallTime: 0, step: 0 });
+    this.setState({ overallTime: 0, step: 0, score: 0, userQuizAnswers: [] });
   }
 
   addUserIDFromTokenToState() {
