1a37824e8ee76df1db537cb6b9e01ed6a978e5fe
Alexandra Santos <alexandraps17@gmail.com>
2020-02-15 16:57:16 +0000

    started search bar

--- src/Learners/Documentation/Documentation.js
+++ src/Learners/Documentation/Documentation.js
@@ -1,37 +1,74 @@
-import React, { useContext } from 'react';
+import React, { useContext, Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import ReactHtmlParser from 'react-html-parser';
 import PropTypes from 'prop-types';
+import { render } from '@testing-library/react';
 import LanguagesContext from '../../contexts/languages-context';
 import translations from '../../i18n/translations';
 
 
-const Documentation = ({ documentation }) => {
-  const { currentLanguage } = useContext(LanguagesContext);
+class Documentation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+    };
+  }
 
-  return (
-    <div>
-      <Container>
-        <h1>{translations[currentLanguage].Documentation.Title}</h1>
-        <hr />
-        {
-        documentation.map((doc) => (
-          <div key={doc.id}>
-            <h2>{doc.title}</h2>
-            <p>{ReactHtmlParser(doc.content)}</p>
-          </div>
-        ))
+  handleChange = (e) => {
+    this.setState({query: e.target.value});
+  }
+
+  handleSearch = () => {
+    let query = new RegExp(this.state.query, 'gi');  
+
+    this.props.documentation.map((doc) => {
+      if(doc.title.match(query) === null){
+          console.log('hide this', doc.title)
+        }else{
+          console.log('this matches', doc.title)
+          } 
+          
+    });
+    console.log('i was clicked')
+  }
+
+  render() {
+    // const { currentLanguage } = useContext(LanguagesContext);
+    const { documentation } = this.props;
+    const { query } = this.state;
+    return (
+      <LanguagesContext.Consumer>
+        {({ currentLanguage }) => (
+          <Container>
+            <h1>{translations[currentLanguage].Documentation.Title}</h1>
+            <div>
+              <input type="text" value={query} onChange={this.handleChange} />
+              <input type="submit" value="Search" onClick={this.handleSearch} />
+            </div>
+            <hr />
+            {
+            documentation.map((doc) => (
+              <article key={doc.id}>
+                <header>
+                  <h1>{doc.title}</h1>
+                </header>
+                <div>{ReactHtmlParser(doc.content)}</div>
+              </article>
+            ))
           }
-        <Link to="/learners/quiz_list">
-          <button type="submit" className="btn">
-            {translations[currentLanguage].Documentation.Button}
-          </button>
-        </Link>
-      </Container>
-    </div>
-  );
-};
+            <Link to="/learners/quiz_list">
+              <button type="submit" className="btn">
+                {translations[currentLanguage].Documentation.Button}
+              </button>
+            </Link>
+          </Container>
+        )}
+      </LanguagesContext.Consumer>
+    );
+  }
+}
 
 Documentation.propTypes = {
   documentation: PropTypes.arrayOf(
