66fc94333a22d10406da527fb970c2303ecfffb3
Alexandra Santos <alexandraps17@gmail.com>
2020-02-16 17:11:59 +0000

    styling-learner-docs

--- src/Learners/Documentation/Documentation.js
+++ src/Learners/Documentation/Documentation.js
@@ -1,18 +1,19 @@
-import React, { useContext, Component } from 'react';
+import React, {  Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import ReactHtmlParser from 'react-html-parser';
 import PropTypes from 'prop-types';
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
 import LanguagesContext from '../../contexts/languages-context';
 import translations from '../../i18n/translations';
-
+import './Documentation.css';
 
 class Documentation extends Component {
   constructor(props) {
     super(props);
     this.state = {
       query: '',
+      resultDocs: [],
     };
   }
 
@@ -21,20 +22,33 @@ class Documentation extends Component {
   }
 
   handleSearch = () => {
-    let query = new RegExp(this.state.query, 'gi');  
 
-    this.props.documentation.map((doc) => {
-      if(doc.title.match(query) === null){
-          console.log('hide this', doc.title)
-        }else{
-          console.log('this matches', doc.title)
-          } 
-          
-    });
-    console.log('i was clicked')
+    if(this.state.query.length < 1){
+      this.setState({resultDocs: []});  
+    }else if(this.state.query.length >= 1){
+      let query = new RegExp(this.state.query, 'gi');  
+  
+      this.props.documentation.map((doc) => {
+        if(doc.title.match(query) !== null){
+          this.setState((prevState) => ({
+            resultDocs: [...prevState.resultDocs, doc]
+          }));  
+        }
+      });
+      if(!this.state.resultDocs[0]){
+        console.log('no match found')
+      }
+    }
+
+    // if no docs are returned say ooops couldn't find anything 
   }
 
+  pickDocs = () => this.state.resultDocs[0] ? this.state.resultDocs : this.props.documentation
+    // return this.props.documentation
+
+
   render() {
+    console.log('this.state.resultDocs', this.state.resultDocs)
     // const { currentLanguage } = useContext(LanguagesContext);
     const { documentation } = this.props;
     const { query } = this.state;
@@ -43,14 +57,20 @@ class Documentation extends Component {
         {({ currentLanguage }) => (
           <Container>
             <h1>{translations[currentLanguage].Documentation.Title}</h1>
-            <div>
+            <hr/>
+            <div className="search-bar">
               <input type="text" value={query} onChange={this.handleChange} />
-              <input type="submit" value="Search" onClick={this.handleSearch} />
+              <button 
+                type="submit" 
+                className="btn" 
+                onClick={this.handleSearch}
+              >
+                Search
+              </button>
             </div>
-            <hr />
             {
-            documentation.map((doc) => (
-              <article key={doc.id}>
+            this.pickDocs().map((doc) => (
+              <article key={doc.id} className="single-doc">
                 <header>
                   <h1>{doc.title}</h1>
                 </header>
