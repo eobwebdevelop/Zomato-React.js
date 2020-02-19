2bdea7f9e0f73be59ef6e75172e7e5d38e06aa07
Edward Watson <e@edwardwatson.com|>
2020-02-19 10:50:22 +0000

    add_back_button_to_bottom_of_faq_rather_than_home

--- src/Learners/FAQ/FAQ.js
+++ src/Learners/FAQ/FAQ.js
@@ -1,12 +1,13 @@
-import React, { useContext } from 'react';
-import { Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import FaqCard from './FaqCard';
-import LanguagesContext from '../../contexts/languages-context';
-import translations from '../../i18n/translations';
+import React, { useContext } from "react";
+import { Container } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+import FaqCard from "./FaqCard";
+import LanguagesContext from "../../contexts/languages-context";
+import translations from "../../i18n/translations";
 
 const FAQ = ({ learnerFaq }) => {
   const { currentLanguage } = useContext(LanguagesContext);
+  let history = useHistory();
   return (
     <>
       <div>
@@ -14,21 +15,21 @@ const FAQ = ({ learnerFaq }) => {
           <h1>{translations[currentLanguage].FAQ.Title}</h1>
           <hr />
 
-          {learnerFaq.map((el) => (
-            <FaqCard
-              key={el.id}
-              Faq={el.faq_question}
-              FaqC={el.content}
-            />
+          {learnerFaq.map(el => (
+            <FaqCard key={el.id} Faq={el.faq_question} FaqC={el.content} />
           ))}
-          <Link to="/learners/quiz_list">
-            <button type="submit" className="btn">
-              {translations[currentLanguage].Documentation.Button}
-            </button>
-          </Link>
+
+          <button
+            type="submit"
+            className="btn"
+            onClick={() => history.goBack()}
+          >
+            {translations[currentLanguage].Documentation.Button}
+          </button>
         </Container>
       </div>
     </>
   );
 };
+
 export default FAQ;
